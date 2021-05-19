240ff5c29435f0c3cc6274b768006915d772bebf
Tobias Løfgren <tobloef@gmail.com>
2021-05-19 22:10:24 +0200

    Add sketch routing

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import * as art from "./art.js";
 import * as params from "./params.js";
-import * as sketch from "./sketches/mask.js";
+import * as mask from "./sketches/mask.js";
+import * as fadingCircles from "./sketches/fading-circles.js";
 
 if (/Mobi/.test(navigator.userAgent)) {
 	alert("The website doesn't work very well in mobile browsers. Sorry about that!")
 }
+
+const sketchMap = {
+	mask,
+	fadingCircles,
+};
+const sketchName = new URLSearchParams(window.location.search).get("sketch");
+const sketch = sketchMap[sketchName] || mask;
 
 const mainCtx = document
 	.querySelector(".main canvas")
